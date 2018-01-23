3a346e40f6dc2e9d13eff4d1032fdd175e83b5ab
eacaps <eacaps@gmail.com>
2018-01-23 16:04:01 -0500

    break out parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import TcxParser from 'tcx';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import TcxFileParser from './tcxfileparser';
 import Utils from './utils';
 import SpeedMap from './speedmap';
 import ScaleComponent from './scalecomponent';
@@ -28,9 +28,7 @@ document.getElementById('file').onchange = (event) => {
     var parser = new DOMParser();
     var doc = parser.parseFromString(fr.result, "text/xml");
     // a tcx file dom, via xmldom
-    const geojson = TcxParser(doc);
-    console.log(geojson);
-    map.processGeojson(geojson);
+    new TcxFileParser().addToMap(doc, map);
   };
   fr.readAsText(file);
 }
@@ -39,8 +37,8 @@ document.getElementById('file').onchange = (event) => {
 fetch('../data/activity_925308754.geojson').then((response) => {
   return response.text();
 }).then((text) => {
-  const geojson = JSON.parse(text);
-  map.processGeojson(geojson);
+    const geojson = JSON.parse(text);
+    new TcxFileParser().processFeatures(geojson, map);
 });
 
 const scale_element = document.getElementById('scale');
